d970ed2f5d46918f3cce2cbeaa2948cafd6a8946
Sheikh <kenneth.sheikh@hotmail.com>
2024-01-15 13:32:01 +0100

    new task

--- script.js
+++ script.js
@@ -1,22 +1,48 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const counterContainer = document.querySelector('.counter');
+// document.addEventListener('DOMContentLoaded', function () {
+//     const counterContainer = document.querySelector('.counter');
 
-    if (counterContainer) {
-        let timeLeft = 10;
+//     if (counterContainer) {
+//         let timeLeft = 10;
 
-        function updateCounter() {
-            counterContainer.innerHTML = timeLeft;
+//         function updateCounter() {
+//             counterContainer.innerHTML = timeLeft;
 
-            if (timeLeft > 0) {
-                setTimeout(function () {
-                    timeLeft--;
-                    updateCounter();
-                }, 1000);
-            } else {
-                counterContainer.innerHTML = "Countdown finished!";
-            }
-        }
+//             if (timeLeft > 0) {
+//                 setTimeout(function () {
+//                     timeLeft--;
+//                     updateCounter();
+//                 }, 1000);
+//             } else {
+//                 counterContainer.innerHTML = "Countdown finished!";
+//             }
+//         }
 
-        updateCounter();
+//         updateCounter();
+//     }
+// });
+
+document.addEventListener("DOMContentLoaded", function () {
+  const counterContainer = document.querySelector(".counter");
+
+  if (counterContainer) {
+    let timeLeft = 10;
+
+    function updateCounter() {
+      if (timeLeft > 6) {
+        counterContainer.innerHTML = timeLeft;
+      } else {
+       
+        counterContainer.innerHTML = `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis ac egestas arcu, quis fringilla felis. Nullam at libero at justo tempor aliquet id at ante. Mauris sed congue metus, scelerisque venenatis massa. Etiam et euismod arcu, sit amet semper arcu. Mauris laoreet tortor ut neque scelerisque sodales. Sed mi sapien, fringilla et condimentum lacinia, blandit sit amet tellus. In lacus massa, ornare vel felis at, vestibulum tincidunt ipsum. Cras sit amet molestie orci. Maecenas efficitur, arcu tincidunt porttitor lacinia, lacus arcu finibus felis, in maximus tortor elit et tellus. Curabitur eu mauris justo. Donec consequat ac urna sed euismod.`;
+      }
+
+      if (timeLeft > 0) {
+        setTimeout(function () {
+          timeLeft--;
+          updateCounter();
+        }, 1000);
+      }
     }
+
+    updateCounter();
+  }
 });
